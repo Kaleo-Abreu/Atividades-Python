e826b89dfa7c4557fd58eda4458109118406b487
Kaleo-Abreu <kaleo.abreu.2009@gmail.com>
2025-11-28 15:07:24 -0300

    Desafio: Calculadora do Amor

---
+++ CalculadoraDoAmor.py
@@ -0,0 +1,30 @@
+print("Calculadora do Amor")
+print("<3 <3 <3 <3 <3")
+
+nome1 = input("Digite o nome da 1ª pessoa: ")
+nome2 = input("Digite o nome da 2ª pessoa: ")
+
+
+nomes = nome1.lower() + nome2.lower()
+
+placar = 0
+
+for char in nomes:
+    if char in "aeiou":
+        placar += 5
+    
+
+    if char in "amor":
+        placar += 10
+
+print("\nSeu placar de compatibilidade:", placar)
+
+
+if placar < 10:
+    print("Nunca vai dar certo!")
+elif placar < 50:
+    print("Talvez com muito esforço!")
+elif placar < 100:
+    print("Vocês formam um belo casal! <3")
+else:
+    print("Vocês terão um relacionamento muito intenso! <3 <3 <3")
